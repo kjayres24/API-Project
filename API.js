a37eeffaf8dbfa32ab8c295665556ac4e33f3ae1
kjayres24 <katieayres24@gmail.com>
2019-03-18 17:18:41 -0400

    3/18/2019

--- API.js
+++ API.js
@@ -4,40 +4,50 @@ const choice = document.getElementById('ddlViewBy');
 const section = document.querySelector('section');
 console.log(choice.value);
 
+const submitBtn = document.getElementById('btn');
 
-fetch(baseURL)
-    .then(results => {
-        return results.json()
-    })
-    .then(json => {
-        displayPeople(json)
-    })
+submitBtn.addEventListener("click", fetchResults);
+
+
+
+
+
 
 function displayPeople(data) {
-    let film = data;
-    console.log(data);
-
-
-    //for (film of data) {
-    //console.log(film.title);
-    // console.log(film.description);
-    //}
-
-
-    for (let i = 0; i < film.length; i++) {
-        let header = document.createElement('h2');
-        let para = document.createElement('p');
-
-        let current = film[i];
-        console.log("Current:", current);
-
-        header.textContent = current.title;
-        para.textContent = current.description;
-
-        film.appendChild(header);
-        film.appendChild(para);
-        section.appendChild(film);
+    while (section.firstChild) {
+        section.removeChild(section.firstChild);
+        let film = data;
+        console.log(data);
     }
 
+    let header = document.createElement('h2');
+    let para = document.createElement('p');
+
+    header.textContent = data[choice.value].title;
+    para.textContent = data[choice.value].description;
+
+    section.appendChild(header);
+    section.appendChild(para);
+    //section.appendChild(film);
+
+
 }
+
+
+
+function fetchResults(e) {
+
+    fetch(baseURL)
+        .then(results => {
+            return results.json();
+        })
+        .then(json => {
+            displayPeople(json);
+            console.log(json);
+        })
+}
+
+
+
+
 
